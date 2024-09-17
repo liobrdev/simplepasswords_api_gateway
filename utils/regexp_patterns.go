968d1979c679355e1afab0185a7bf1b13f670831
liobrdev <contact@liobr.dev>
2024-09-17 08:43:53 -0400

    Fixes slug regex.

--- utils/regexp_patterns.go
+++ utils/regexp_patterns.go
@@ -7,7 +7,7 @@ var (
 	NameRegexp			 			 = regexp.MustCompile(`^[\\u00BF-\\u1FFF\\u2C00-\\uD7FF\w \-\,\.\']{1,50}$`)
 	PhoneRegexp			 			 = regexp.MustCompile(`^\+[1-9]\d{1,14}$`)
 	HexEncodedKeyRegexp		 = regexp.MustCompile(`^[A-Fa-f0-9]{64}$`)
-	SlugRegexp       			 = regexp.MustCompile(`^[\w-]{32}$`)
+	SlugRegexp       			 = regexp.MustCompile(`^[\w-]{16}$`)
 	TokenRegexp      			 = regexp.MustCompile(`^[\w-]{80}$`)
 	RowsRegexp       			 = regexp.MustCompile(`^result.RowsAffected \([0-9]+\) > 1$`)
 	AuthHeaderRegexp 			 = regexp.MustCompile(`^[Tt]oken [\w-]{80}$`)
